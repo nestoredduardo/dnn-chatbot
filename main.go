418ddd502a6fc4537218da8a029ab46076c56874
NedduJS <nmamanipantoja@gmail.com>
2021-08-30 11:31:12 -0500

    First training

--- main.go
+++ main.go
@@ -3,26 +3,29 @@ package main
 import (
 	"bufio"
 	"errors"
+	"fmt"
 	"log"
 	"math"
 	"math/rand"
 	"os"
+	"regexp"
+	"strings"
 	"time"
+	"unicode"
 
+	"golang.org/x/text/runes"
+	"golang.org/x/text/transform"
+	"golang.org/x/text/unicode/norm"
 	"gonum.org/v1/gonum/floats"
 	"gonum.org/v1/gonum/mat"
 )
 
-func main() {
-
-}
-
 type neuralNet struct {
 	config  neuralNetConfig
-	wHidden mat.Dense
-	bHidden mat.Dense
-	wOut    mat.Dense
-	bOut    mat.Dense
+	wHidden *mat.Dense
+	bHidden *mat.Dense
+	wOut    *mat.Dense
+	bOut    *mat.Dense
 }
 
 type neuralNetConfig struct {
@@ -33,8 +36,52 @@ type neuralNetConfig struct {
 	learningRate  float64
 }
 
-func newNetwork(config neuralNetConfig) neuralNet {
-	return neuralNet{config: config}
+func main() {
+	inputs, labels := makeInputAndLabels("./chats.txt")
+
+	config := neuralNetConfig{
+		inputNeurons:  28,
+		outputNeurons: 7,
+		hiddenNeurons: 28,
+		numEpochs:     3000,
+		learningRate:  0.05,
+	}
+
+	network := newNetwork(config)
+	if err := network.train(inputs, labels); err != nil {
+		log.Fatal(err)
+	}
+
+	predictions, err := network.predict(inputs)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var truePosNeg int
+	numPreds, _ := predictions.Dims()
+	for i := 0; i < numPreds; i++ {
+
+		labelRow := mat.Row(nil, i, labels)
+		var prediction int
+		for idx, label := range labelRow {
+			if label == 1.0 {
+				prediction = idx
+				break
+			}
+		}
+
+		if predictions.At(i, prediction) == floats.Max(mat.Row(nil, i, predictions)) {
+			truePosNeg++
+		}
+	}
+
+	accuracy := float64(truePosNeg) / float64(numPreds)
+
+	fmt.Printf("\nAccuracy = %0.2f\n\n", accuracy)
+}
+
+func newNetwork(config neuralNetConfig) *neuralNet {
+	return &neuralNet{config: config}
 }
 
 func sigmoid(x float64) float64 {
@@ -76,10 +123,10 @@ func (nn *neuralNet) train(x, y *mat.Dense) error {
 		return err
 	}
 
-	nn.wHidden = *wHidden
-	nn.bHidden = *bHidden
-	nn.wOut = *wOut
-	nn.bOut = *bOut
+	nn.wHidden = wHidden
+	nn.bHidden = bHidden
+	nn.wOut = wOut
+	nn.bOut = bOut
 
 	return nil
 }
@@ -88,6 +135,7 @@ func (nn *neuralNet) backpropagate(x, y, wHidden, bHidden, wOut, bOut, output *m
 	for i := 0; i < nn.config.numEpochs; i++ {
 
 		hiddenLayerInput := new(mat.Dense)
+		fmt.Println(x, wHidden)
 		hiddenLayerInput.Mul(x, wHidden)
 		addBHidden := func(_, col int, v float64) float64 { return v + bHidden.At(0, col) }
 		hiddenLayerInput.Apply(addBHidden, hiddenLayerInput)
@@ -178,7 +226,7 @@ func (nn neuralNet) predict(x *mat.Dense) (*mat.Dense, error) {
 	output := new(mat.Dense)
 
 	hiddenLayerInput := new(mat.Dense)
-	hiddenLayerInput.Mul(x, &nn.wHidden)
+	hiddenLayerInput.Mul(x, nn.wHidden)
 	addBHidden := func(_, col int, v float64) float64 { return v + nn.bHidden.At(0, col) }
 	hiddenLayerInput.Apply(addBHidden, hiddenLayerInput)
 
@@ -187,7 +235,7 @@ func (nn neuralNet) predict(x *mat.Dense) (*mat.Dense, error) {
 	hiddenLayerActivations.Apply(applySigmoid, hiddenLayerInput)
 
 	outputLayerInput := new(mat.Dense)
-	outputLayerInput.Mul(hiddenLayerActivations, &nn.wOut)
+	outputLayerInput.Mul(hiddenLayerActivations, nn.wOut)
 	addBOut := func(_, col int, v float64) float64 { return v + nn.bOut.At(0, col) }
 	outputLayerInput.Apply(addBOut, outputLayerInput)
 	output.Apply(applySigmoid, outputLayerInput)
@@ -197,6 +245,7 @@ func (nn neuralNet) predict(x *mat.Dense) (*mat.Dense, error) {
 
 func makeInputAndLabels(fileName string) (*mat.Dense, *mat.Dense) {
 	var X []string
+	var y = [28]int{0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6}
 
 	file, err := os.Open(fileName)
 	if err != nil {
@@ -214,6 +263,96 @@ func makeInputAndLabels(fileName string) (*mat.Dense, *mat.Dense) {
 		log.Fatal(err)
 	}
 
-	inputsData := make([]float64, 28*len(X))
-	labelsData := make([]float64, 7*len(X))
+	var nColumns = 28
+	var nLabels = 7
+
+	var inputsData []float64
+	var labelsData []float64
+
+	var cleanedTextArray []string
+
+	for i := 0; i < len(X); i++ {
+		cleanedTextArray = append(cleanedTextArray, clear_text(X[i]))
+	}
+
+	for i := 0; i < len(cleanedTextArray); i++ {
+		var cleanedText string = cleanedTextArray[i]
+		var textInt []float64
+		for j := 0; j < len(cleanedText); j++ {
+			var char = string(cleanedText[j])
+			textInt = append(textInt, float64(word_to_idx[char]))
+		}
+		if len(textInt) < nColumns {
+			diff := nColumns - len(textInt)
+			for i := 0; i < diff; i++ {
+				textInt = append(textInt, 0)
+			}
+		}
+		inputsData = append(inputsData, textInt...)
+	}
+
+	for i := 0; i < len(y); i++ {
+		var labelHot []float64
+		for j := 0; j < nLabels; j++ {
+			if y[i] == j || y[i] == nLabels {
+				labelHot = append(labelHot, 1)
+			} else {
+				labelHot = append(labelHot, 0)
+			}
+		}
+		labelsData = append(labelsData, labelHot...)
+	}
+
+	inputs := mat.NewDense(len(cleanedTextArray), nColumns, inputsData)
+	labels := mat.NewDense(len(cleanedTextArray), nLabels, labelsData)
+	return inputs, labels
+}
+
+func clear_text(text string) string {
+	t := transform.Chain(norm.NFD, runes.Remove(runes.In(unicode.Mn)), norm.NFC)
+	output, _, e := transform.String(t, text)
+	if e != nil {
+		panic(e)
+	}
+
+	reg, err := regexp.Compile("[^a-zA-Z!?]+")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	text2 := reg.ReplaceAllString(output, "")
+	text2 = strings.Replace(text2, "?", "", -1)
+	text2 = strings.ToLower(text2)
+	return text2
+}
+
+var word_to_idx = map[string]int{
+	"a": 1,
+	"b": 2,
+	"c": 3,
+	"d": 4,
+	"e": 5,
+	"f": 6,
+	"g": 7,
+	"h": 8,
+	"i": 9,
+	"j": 10,
+	"k": 11,
+	"l": 12,
+	"m": 13,
+	"n": 14,
+	"ñ": 15,
+	"o": 16,
+	"p": 17,
+	"q": 18,
+	"r": 19,
+	"s": 20,
+	"t": 21,
+	"u": 22,
+	"v": 23,
+	"w": 24,
+	"x": 25,
+	"y": 26,
+	"z": 27,
 }
